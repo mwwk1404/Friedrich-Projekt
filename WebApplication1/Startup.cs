991757da42e93b223a8471eb53603d0a209020e3
Moritz <moritz.klass@icloud.com>
2021-02-20 10:46:14 +0100

    altes Programm gelöscht neue Version hochgeladen

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1.Startup))]
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
